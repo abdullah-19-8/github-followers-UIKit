10204b38814113cc57354b04ed43bf21eccd1018
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2024-01-20 00:29:10 +0300

    Downloading & Caching Avatar Images

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,12 +5,12 @@
 //  Created by Abdulla Ahmad on 1/16/24.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
-    static let shared = NetworkManager()
-
-    let baseURL = "https://api.github.com/users/"
+    static let shared   = NetworkManager()
+    private let baseURL         = "https://api.github.com/users/"
+    let cache           = NSCache<NSString, UIImage>()
     
     private init() {}
     
