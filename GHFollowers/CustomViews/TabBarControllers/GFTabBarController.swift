76720ae0dbaecf3af008758ca507e3a34f5d3d1c
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2024-02-03 09:55:10 +0300

    refactoring sceneDelegate

---
+++ GHFollowers/CustomViews/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by Abdulla Ahmad on 2/3/24.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers                 = [createSearchNC(), createFavoriteNC()]
+        
+    }
+    
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchViewController()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoriteNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListViewController()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
--- GHFollowers/Screens/FavoritesListViewController.swift
+++ GHFollowers/Screens/FavoritesListViewController.swift
@@ -20,6 +20,7 @@ class FavoritesListViewController: UIViewController {
     }
     
     
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         getFavorites()
